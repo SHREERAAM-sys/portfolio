87e2f40a50e144266d27a4b2b8764db4fcb4be63
shreeraam-sys <shreeraamr@gmail.com>
2024-03-15 23:21:33 -0400

    formated code

--- professional-portfolio/src/components/about.tsx
+++ professional-portfolio/src/components/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
   return (
     <motion.section
       //ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-20 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
@@ -19,30 +19,12 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        I'm Shreeraam Ramachandran, an Information Systems graduate student at
+        Northeastern University with two years of experience as a software
+        developer . I have been an essential part of designing and architecting
+        cloud products, developing efficiency-boosting frameworks, and possess
+        expertise in system design, cloud computing, database management, and
+        secure software development
       </p>
     </motion.section>
   );
